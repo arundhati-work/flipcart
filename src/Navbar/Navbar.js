5707c056fc9622968a2499ce047291e8212767e0
Arundhati <arundhatib.work@gmail.com>
2023-10-05 08:35:01 +0530

    Modal responsive

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -8,8 +8,6 @@ function Navbar(){
             <p>Flipcart</p>
         </div>
         <div className='Links'>
-            <a href='#'>Become a seller</a>
-            <a href='#'>New User?</a>
             <a href='#'>Cart (0)</a>
         </div>
     </div>
