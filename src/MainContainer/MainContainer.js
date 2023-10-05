ef4688c1bab4193257711463f8b74e614a607cca
Arundhati <arundhatib.work@gmail.com>
2023-10-05 10:47:49 +0530

    Overall quantity working

--- src/MainContainer/MainContainer.js
+++ src/MainContainer/MainContainer.js
@@ -7,15 +7,16 @@ import fashion from '../data/FashionData';
 import furnitures from '../data/FurnitureData';
 import Tray from '../Tray/Tray';
 import Footer from '../Footer/Footer';
+import React, {useState} from 'react';
 
 function MainContainer() {
+    const [overallQuant, setOverallQuant] = useState(0);
     return (
         <div className="main-container">
-            <Navbar/> 
+            <Navbar overallQuant={overallQuant}/> 
             <div className='categories-section'>
                 <h1>Categories</h1>
                 <div className="categories-container">
-                    
                     {
                         CategoryData.map((category)=> {
                             return <Category key={category.id} label={category.label} image={category.image}/>
@@ -24,9 +25,9 @@ function MainContainer() {
                 </div>
             </div>
             <div className='trays-container'>
-                <Tray category="Electronics" object={electronics}/>
-                <Tray category="Fashion" object={fashion}/>
-                <Tray category="Furniture" object={furnitures}/>
+                <Tray category="Electronics" object={electronics} overallQuant={overallQuant} handleOverallQuant={(val)=>setOverallQuant(val)}/>
+                <Tray category="Fashion" object={fashion} handleOverallQuant={(val)=>setOverallQuant(val)}/>
+                <Tray category="Furniture" object={furnitures} handleOverallQuant={(val)=>setOverallQuant(val)}/>
             </div>
             <Footer/>
         </div>
